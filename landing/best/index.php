9351ca143b27ea04367bd3e8a1fa528588ba69d3
Kartakov-P <Kartakov-P@gaz-is.ru>
2019-06-13 16:26:19 +0300

    landing best

---
+++ landing/best/index.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Продающий сайт заказать - Семь Ветров, веб-студия в Ульяновске</title>
+    <script src="/local/templates/new-itin/js/jquery-1.11.3.min.js"></script>
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="keywords" content="Семь Ветров, Ульяновск, веб-студия, лендинг недвижимость, заказать, лендинг, недорого, ульяновск" />
+    <meta name="description" content="Заказать лендинг, продающий сайт в Ульяновске, недорого. Семь Ветров - веб-студия в Ульяновске" />
+    <link href="/bitrix/js/main/core/css/core.min.css" type="text/css" rel="stylesheet" />
+    <link href="/local/templates/.default/components/bitrix/system.auth.form/clients_auth/style.css" type="text/css" data-template-style="true" rel="stylesheet" />
+    <link href="/local/templates/new-itin/template_styles.css" type="text/css" data-template-style="true" rel="stylesheet" />
+    <link rel="stylesheet" href="/local/templates/.default/css/interface.css">
+    <link href="/local/templates/new-itin/images/system/favicon.ico" rel="icon">
+    <link href="/local/templates/new-itin/css/bootstrap.css" rel="stylesheet">
+    <link href="/local/templates/new-itin/css/bootstrap-theme.css" rel="stylesheet">
+    <link href="/local/templates/new-itin/js/slick/slick.css" rel="stylesheet">
+    <link href="/local/templates/new-itin/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/local/templates/new-itin/css/animate.min.css" rel="stylesheet">
+    <link href="/local/templates/new-itin/css/media.css" rel="stylesheet">
+    <link href="/local/templates/new-itin/js/formstyler/jquery.formstyler.css" rel="stylesheet">
+    <link href="/local/templates/new-itin/template_styles.css" rel="stylesheet">
+    <!--Global variables-->
+    <link rel="stylesheet" href="/landing/best/css/header.css">
+    <link rel="stylesheet" href="/landing/best/css/main.css">
+    <link rel="stylesheet" href="/landing/best/css/vendor.css">
+    <link rel="stylesheet" href="/landing/best/css/box.css">
+    <link href='https://fonts.googleapis.com/css?family=PT+Sans:400,700&amp;subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+    <style>
+        .header-topline {
+            background: linear-gradient(#610e3c 26%, #450823);
+            position: fixed;
+            left: 0;
+            top: 0;
+            right: 0;
+            padding-top: 16px;
+            padding-bottom: 18px;
+            z-index: 10;
+        }
+
+        .header-toplinefixed .header-topline {
+            padding-top: 5px;
+            padding-bottom: 5px;
+            background: linear-gradient(#610e3c 26%, #450823);
+        }
+    </style>
+</head>
+
+<body>
+    <?php include $_SERVER['DOCUMENT_ROOT']."/local/menu.php"; ?>
+    <!-- content -->
+    <div class="b-section b-section--best--1">
+        <div class="container2">
+            <div class="b-section__video b-section__video--left b-section__video--bgc_best">
+                <video poster="" autoplay loop muted class="b-bgvideo">
+						<source src="video/best.mp4" type="video/mp4"></source>
+					</video>
+            </div>
+            <div class="b-section__text b-section__text--right">
+                <div class="b-text">
+                    <span class="b-section__hb">
+							<a href="/" >главная</a> /
+                            <a title="Заказать промо сайт или лендинг" href="/landing/">продающие сайты</a> /
+                            <a title="Заказать промо сайт или лендинг" href="/landing/best/">премиум</a>
+                    </span>
+
+                    <div class="b-text__top-line b-text__top-line--white">
+                        <br /> категория сайтов </div>
+                    <div style="font-size:80px" class="b-text__name b-text__name--white">
+                        Премиум
+                    </div>
+                    <div class="b-text__sub-name b-text__sub-name--white"> </div>
+                    <div class="b-text__description b-text__description--white">
+                        <ul>
+                            <!--li>Cтоимость: <span>90 000р - 250 000р</span></li-->
+                            <li>Срок разработки: <span>3-5 </span>недель</li>
+                        </ul>
+                    </div>
+                    <a target="_blank" href="#callback2" class="fancybox b-text__link b-text__link--bg_white ">Заказать сайт </a>
+                    <a target="_blank" href="#callback3" class="fancybox b-text__link b-text__link--bg_white2 ">Консультация </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="b-section b-section--gadget--4">
+        <div class="container2">
+            <div class="b-section__video b-section__video--right b-section__video--bgc_industry">
+                <video poster="img/versal.gif" autoplay loop muted class="b-bgvideo">
+						<source src="http://tornado.sewind.ru/!!!/media/video/intro.mp4" type="video/mp4"></source>
+					</video>
+            </div>
+            <div class="b-section__text b-section__text--left">
+                <div class="b-text">
+                    <div class="b-text__top-line b-text__top-line--white"> дизайн<br /> лендинга</div>
+                    <div class="b-text__name b-text__name--white">
+                        phone<br /> fly
+                    </div>
+                    <div class="b-text__sub-name b-text__sub-name--white"> флагманская модель </div>
+                    <a target="_blank" href="http://tornado.sewind.ru" class="external-reference b-text__link b-text__link--bg_white b-text__link--color_red">смотреть сайт</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="b-section b-section--programs--4">
+        <div class="container2">
+            <div class="b-section__video b-section__video--left b-section__video--bgc_kaspersky">
+                <video poster="img/versal.gif" autoplay loop muted class="b-bgvideo">
+						<source src="video/kaspersky.mp4" type="video/mp4"></source>
+					</video>
+            </div>
+            <div class="b-section__text b-section__text--right">
+                <div class="b-section__text b-section__text--right">
+                    <div class="b-text">
+                        <div class="b-text__top-line b-text__top-line--white "> <br />Создание <br /> landing page</div>
+                        <div class="b-text__name b-text__name--white">
+                            <img src="img/programs/kaspersky.png" /> <br />
+                        </div>
+                        <div class="b-text__sub-name b-text__sub-name--white">
+                            система защиты от вирусов<br /> спама и хакерских атак
+                        </div>
+                        <a target="_blank" href="/landing/programs/kaspersky" class="b-text__link b-text__link--bg_white">смотреть сайт</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="b-section b-section--bu--2">
+        <div class="container2">
+            <div class="b-section__video b-section__video--right b-section__video--bgc_blue">
+                <video poster="img/versal.gif" autoplay loop muted class="b-bgvideo">
+						<source src="video/sneg.mp4" type="video/mp4"></source>
+					</video>
+            </div>
+            <div class="b-section__text b-section__text--left">
+                <div class="b-text">
+                    <div class="b-text__top-line b-text__top-line--white">Создание лендинга</div>
+                    <div class="b-text__name b-text__name--white">
+                        luxury house<br/>SNEGIRI
+                    </div>
+                    <div class="b-text__sub-name b-text__sub-name--white"></div>
+                    <div class="b-text__description b-text__description--white">
+                        Элитные квартиры в парке<br/>на кутузовском проспекте
+                    </div>
+                    <a target="_blank" href="/landing/bu/snegiri" class="b-text__link b-text__link--bg_white ">смотреть сайт</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="b-section b-section--bu--3 ">
+        <div class="container2">
+            <div class="b-section__text b-section__text--left">
+                <div class="b-text">
+                    <div class="b-text__top-line b-text__top-line--white">Создание лендинга</div>
+                    <div class="b-text__name b-text__name--white">
+                        luxury house
+                        <div class="nesk"> neskuchnyy</div>
+                    </div>
+                    <div class="b-text__description b-text__description--white">
+                        Элитные квартиры <br/>на Ленинском проспекте<br/>
+                    </div>
+                    <a target="_blank" href="/landing/bu/neskuchnyy/" class="b-text__link b-text__link--bg_white ">смотреть сайт</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="b-section b-section--food--2">
+        <div class="container2">
+            <div class="b-section__text">
+                <div class="b-text">
+                    <div style="margin-top: 34px;" class="b-text__top-line b-text__top-line--white">Создание лендинга</div>
+                    <div class="b-text__name b-text__name--white">
+                        Perfecto <br /> Naturica
+                    </div>
+                    <div style="color: #fff;" class="b-text__sub-name b-text__sub-name--black">диетические пельмени</div>
+                    <div class="b-text__description b-text__description--black">
+                    </div>
+                    <a target="_blank" href="/landing/food/perfecto" class="b-text__link b-text__link--bg_white">смотреть сайт</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="b-section b-section--food--3">
+        <div class="container2">
+            <div class="b-section__video b-section__video--left b-section__video--bgc_red">
+                <video poster="" autoplay loop muted class="b-bgvideo">
+						<source src="video/blander.mp4" type="video/mp4"></source>
+					</video>
+            </div>
+            <div class="b-section__text b-section__text--right">
+                <div class="b-text">
+                    <div class="b-text__top-line b-text__top-line--white">Создание<br />Landing Page</div>
+                    <div class="b-text__name b-text__name--white">
+                        Blender<br /> Shop
+                    </div>
+                    <div class="b-text__sub-name b-text__sub-name--white">
+                        погружной блендер<br /> с аксессуарами</div>
+
+                    <a target="_blank" href="/landing/food/blender" class="b-text__link b-text__link--bg_white b-text__link--color_red">смотреть сайт</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="b-section b-section--bu--10">
+        <div class="container2">
+            <div class="b-section__text b-section__text--left">
+                <div class="b-text">
+                    <div class="b-text__top-line b-text__top-line--black"><br /> </div>
+                    <div class="b-text__name b-text__name--black">
+                        <img src="img/bu/abada.png" /> <br />
+                    </div>
+                    <div class="b-text__sub-name b-text__sub-name--black">качественный ремонт <br /> для вашего дома </div>
+                    <div class="b-text__description b-text__description--black">
+                    </div>
+                    <a target="_blank" href="/landing/repair/abada/" class="b-text__link b-text__link--bg_white b-text__link--color_purple">смотреть сайт</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+    <div class="b-section b-section--gadget--2">
+        <div class="container2">
+            <div class="b-section__text b-section__text--left">
+                <div class="b-text b-text--w440">
+                    <div class="b-text__top-line b-text__top-line--white">Создание <br />линии лендингов</div>
+                    <div class="b-text__name b-text__name--white">
+                        galt<br /> agency
+                    </div>
+                    <div class="b-text__sub-name b-text__sub-name--white">
+                        инновационная реклама</div>
+
+                    <a target="_blank" href="/landing/gadget/galt/panorama.html" class="margin-g b-text__link b-text__link--bg_white b-text__link--color_dark-grey">panorama</a>
+                    <a target="_blank" href="/landing/gadget/galt/promobot.html" class="margin-g b-text__link b-text__link--bg_white b-text__link--color_dark-grey">promobot</a>
+                    <a target="_blank" href="/landing/gadget/galt/video.html" class="margin-g b-text__link b-text__link--bg_white b-text__link--color_dark-grey">videofridge</a>
+                    <a target="_blank" href="/landing/gadget/galt/flight.html" class="margin-g b-text__link b-text__link--bg_white b-text__link--color_dark-grey">flight</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="b-section b-section--interior--4">
+        <div class="container2">
+            <div class="b-section__video b-section__video--left b-section__video--bgc_lawyer">
+                <video poster="img/versal.gif" autoplay loop muted class="b-bgvideo">
+						<source src="video/rost.mp4" type="video/mp4"></source>
+					</video>
+            </div>
+            <div class="b-section__text b-section__text--right">
+                <div class="b-text">
+                    <div class="b-text__top-line b-text__top-line--white"> дизайн<br /> landing page</div>
+                    <div class="b-text__name b-text__name--white">
+                        rost Dream
+                    </div>
+                    <div class="b-text__sub-name b-text__sub-name--white">
+                        диваны и кресла
+                    </div>
+
+                    <a target="_blank" href="/landing/interior/rost" class="b-text__link b-text__link--bg_white b-text__link--color_red">смотреть сайт</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="b-section b-section--programs--8">
+        <div class="container2">
+            <div class="b-section__text b-section__text--left">
+                <div class="b-text">
+                    <div class="b-text__top-line b-text__top-line--white"> <br />Разработка
+                        <br /> landing page</div>
+                    <div class="b-text__name b-text__name--white">
+                        energoport catalog
+                    </div>
+                    <div class="b-text__sub-name b-text__sub-name--white">
+                    </div>
+                    <div class="b-text__description b-text__description--white">
+                        370 000 товаров от 14 000 поставщиков
+                        <br />
+                    </div>
+                    <a target="_blank" href="/landing/programs/energo" class="b-text__link b-text__link--bg_white b-text__link--color_dark-grey">смотреть сайт</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="b-section b-section--lawyer--3">
+        <div class="container2">
+            <div class="b-section__text b-section__text--right">
+                <div class="b-text">
+                    <div class="b-text__top-line b-text__top-line--white "> <br /> landing page</div>
+                    <div class="b-text__name b-text__name--white">
+                        Paprasta Apskaita
+                    </div>
+                    <div class="b-text__sub-name b-text__sub-name--white">
+                        Бухгалтерское и юридическое обслуживание
+                    </div>
+                    <a target="_blank" href="/landing/lawyer/apskaita" class="b-text__link b-text__link--bg_white">смотреть сайт</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /content -->
+    </main>
+    <?php include $_SERVER['DOCUMENT_ROOT']."/local/footer.php"; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT']."/local/orders.php"; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT']."/local/metrics.php"; ?>
+    <script>
+        $(document).ready(function() {
+            $('.js-form-callback-mailto').submit(function(e) {
+                $.ajax({
+                    url: $(this).attr('action'),
+                    data: $(this).serialize() + "&web_form_submit=Отправить",
+                    type: $(this).attr('method'),
+                    dataType: 'json',
+                    success: function(data) {
+                        if (data == 1) {
+                            $('#callback_mailto .js-form-before-send').hide();
+                            $('#callback_mailto .js-form-success').fadeIn();
+                        }
+                    },
+                    error: function(data) {}
+                });
+                e.preventDefault();
+            });
+        });
+    </script>
+    <script src="/local/templates/new-itin/js/jquery.smooth-scroll.min.js"></script>
+    <script src="/local/templates/new-itin/js/jquery.maskedinput.min.js"></script>
+    <script src="/local/templates/new-itin/js/bootstrap.min.js"></script>
+    <script src="/local/templates/new-itin/js/slick/slick.min.js"></script>
+    <script src="/local/templates/new-itin/js/jquery.calc-0.0.3.js"></script>
+    <script src="/local/templates/new-itin/js/formstyler/jquery.formstyler.min.js"></script>
+    <script src="/local/templates/new-itin/js/ammi.js"></script>
+    <script src="/local/templates/new-itin/js/calculator.js"></script>
+    <script src="/local/templates/new-itin/js/calc-sections.js"></script>
+    <script src="/local/templates/.default/js/script.js"></script>
+</body>
+
+</html>
